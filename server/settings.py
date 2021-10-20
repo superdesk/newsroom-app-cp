7ba97738aa6cde61071307c5fdb17ca0e2ed0104
Petr Jasek <jasekpetr@gmail.com>
2021-10-20 14:42:29 +0200

    add theme from newsroom#cp
    CPNHUB-4

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,23 @@
 import pathlib
 
+from newsroom.web.default_settings import CLIENT_CONFIG
+
 SERVER_PATH = pathlib.Path(__file__).resolve().parent
 CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
+
+SITE_NAME = 'CP Newshub'
+COPYRIGHT_HOLDER = 'CP'
+
+SERVICES = [
+    {"name": "Domestic Sport", "code": "t"},
+    {"name": "Overseas Sport", "code": "s"},
+    {"name": "Finance", "code": "f"},
+    {"name": "International News", "code": "i"},
+    {"name": "Entertainment", "code": "e"},
+]
+
+SHOW_USER_REGISTER = False
+
+CLIENT_CONFIG.update({
+    'display_news_only': True  # Displays news only switch in wire
+})
