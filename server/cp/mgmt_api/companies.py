9d1c9b7a18a34f293053db5ccb177082995d5c11
dev-tsoni <tanuj.soni@sourcefabric.org>
2021-11-08 14:11:50 +0530

    CPNHUB-8 ADD: Added public methods to companies and users endpoints.

--- server/cp/mgmt_api/companies.py
+++ server/cp/mgmt_api/companies.py
@@ -4,4 +4,5 @@ import superdesk
 
 def init_app(app):
     CompaniesResource.internal_resource = False
+    CompaniesResource.public_methods = ["GET", "PATCH", "POST", "DELETE"]
     superdesk.register_resource('companies', CompaniesResource, CompaniesService, _app=app)
--- server/cp/mgmt_api/users.py
+++ server/cp/mgmt_api/users.py
@@ -4,4 +4,5 @@ import superdesk
 
 def init_app(app):
     UsersResource.internal_resource = False
+    UsersResource.public_methods = ["GET", "PATCH", "POST", "DELETE"]
     superdesk.register_resource('users', UsersResource, UsersService, _app=app)
