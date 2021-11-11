aa3c2f866080a06c4b397670ae059270b9348575
dev-tsoni <tanuj.soni@sourcefabric.org>
2021-11-11 09:24:28 +0530

    CPNHUB-8 FIX: Removed public_method from companies and users.

--- server/cp/mgmt_api/companies.py
+++ server/cp/mgmt_api/companies.py
@@ -4,5 +4,4 @@ import superdesk
 
 def init_app(app):
     CompaniesResource.internal_resource = False
-    CompaniesResource.public_methods = ["GET", "PATCH", "POST", "DELETE"]
     superdesk.register_resource('companies', CompaniesResource, CompaniesService, _app=app)
