0812b067c58f2ab35961a78601c6edef51c86479
dev-tsoni <tanuj.soni@sourcefabric.org>
2021-11-17 21:14:47 +0530

    CPNHUB-10 ADD: Routes to manage company products

--- server/cp/mgmt_api/companies.py
+++ server/cp/mgmt_api/companies.py
@@ -1,6 +1,7 @@
 from bson import ObjectId
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from newsroom.companies import CompaniesResource, CompaniesService
+from newsroom.utils import get_entity_or_404
 import superdesk
 
 blueprint = Blueprint('companies', __name__)
@@ -13,5 +14,23 @@ def init_app(app):
 
 @blueprint.route('/api/companies/<_id>/products', methods=['GET', 'OPTIONS'])
 def get_company_products(_id):
+    """ Fetches the company products by given id """
     products = superdesk.get_resource_service('products').find({'companies': ObjectId(_id)})
-    return jsonify(list(products))
+    return jsonify(list(products)), 200
+
+
+@blueprint.route('/api/companies/<product_id>/products', methods=['PATCH'])
+def patch_company_products(product_id):
+    """ Updates the company products by given id """
+    get_entity_or_404(ObjectId(product_id), 'products')
+    updates = request.get_json()
+    superdesk.get_resource_service('products').patch(id=ObjectId(product_id), updates=updates)
+    return jsonify({'success': True}), 200
+
+
+@blueprint.route('/api/companies/<product_id>/products', methods=['DELETE'])
+def delete_company_products(product_id):
+    """ Deletes the company products by given id """
+    get_entity_or_404(ObjectId(product_id), 'products')
+    superdesk.get_resource_service('products').delete_action({'_id': ObjectId(product_id)})
+    return jsonify({'success': True}), 200
