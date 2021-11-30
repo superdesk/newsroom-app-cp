b1113beb15054f970316093c0f403534cb967f62
Petr Jasek <jasekpetr@gmail.com>
2021-11-30 11:37:52 +0100

    setup wire aggregations
    CPNHUB-21

--- server/settings.py
+++ server/settings.py
@@ -1,12 +1,13 @@
 import pathlib
-
+from flask_babel import lazy_gettext
 from newsroom.web.default_settings import CLIENT_CONFIG
+
 
 SERVER_PATH = pathlib.Path(__file__).resolve().parent
 CLIENT_PATH = SERVER_PATH.parent.joinpath("client")
 
-SITE_NAME = 'CP Newshub'
-COPYRIGHT_HOLDER = 'CP'
+SITE_NAME = "CP Newshub"
+COPYRIGHT_HOLDER = "CP"
 
 SERVICES = [
     {"name": "Domestic Sport", "code": "t"},
@@ -17,27 +18,80 @@ SERVICES = [
 ]
 
 SHOW_USER_REGISTER = False
-
 NEWS_ONLY_FILTERS = []
 
-LANGUAGES = ['en', 'fr_CA']
-DEFAULT_LANGUAGE = 'en'
+LANGUAGES = ["en", "fr_CA"]
+DEFAULT_LANGUAGE = "en"
 
-CLIENT_CONFIG.update({
-    'display_news_only': False,  # Displays news only switch in wire
+CLIENT_CONFIG.update(
+    {
+        "display_news_only": False,  # Displays news only switch in wire
+        "time_format": "HH:mm",
+        "date_format": "MMM Do, YYYY",
+        "locale_formats": {
+            "en": {
+                "TIME_FORMAT": "HH:mm",
+                "DATE_FORMAT": "MMM Do, YYYY",
+                "COVERAGE_DATE_FORMAT": "MMM Do, YYYY",
+                "COVERAGE_DATE_TIME_FORMAT": "HH:mm MMM Do, YYYYY",
+            },
+            "fr_CA": {
+                "TIME_FORMAT": "HH:mm",
+                "DATE_FORMAT": "MMM Do, YYYY",
+            },
+        },
+    }
+)
 
-    'time_format': 'HH:mm',
-    'date_format': 'MMM Do, YYYY',
-    'locale_formats': {
-        "en": {
-            "TIME_FORMAT": "HH:mm",
-            "DATE_FORMAT": "MMM Do, YYYY",
-            "COVERAGE_DATE_FORMAT": "MMM Do, YYYY",
-            "COVERAGE_DATE_TIME_FORMAT": "HH:mm MMM Do, YYYYY",
-        },
-        "fr_CA": {
-            "TIME_FORMAT": "HH:mm",
-            "DATE_FORMAT": "MMM Do, YYYY",
-        },
+WIRE_GROUPS = [
+    {
+        "field": "source",
+        "label": lazy_gettext("Source"),
     },
-})
+    {
+        "field": "service",
+        "label": lazy_gettext("Wire Category"),
+    },
+    {
+        "field": "subject",
+        "label": lazy_gettext("Index/Subject"),
+    },
+    {
+        "field": "genre",
+        "label": lazy_gettext("Version"),
+    },
+    {
+        "field": "urgency",
+        "label": lazy_gettext("News Value"),
+    },
+]
+
+WIRE_AGGS = {
+    "source": {"terms": {"field": "source", "size": 50}},
+    "service": {"terms": {"field": "service.name", "size": 50}},
+    "subject": {
+        "terms": {
+            "field": "subject.name",
+            "size": 20,
+            "include": [
+                "Print",
+                "Broadcast",
+                "Print / Broadcast",
+                "QuickHit",
+                "NewsBase",
+                "DataSpecials",
+                "Sports",
+                "Politics",
+                "Business",
+                "Health",
+                "Environment",
+                "Science",
+                "Lifestyle",
+                "Religion",
+                "Education",
+            ],
+        }
+    },
+    "genre": {"terms": {"field": "genre.name", "size": 50}},
+    "urgency": {"terms": {"field": "urgency"}},
+}
