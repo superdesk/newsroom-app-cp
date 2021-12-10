b96e2ab1e41d6986e25e90201bab7870798466e8
Petr Jasek <jasekpetr@gmail.com>
2021-12-10 10:17:37 +0100

    update images link label
    CPNHUB-16

--- server/cp/sidenav.py
+++ server/cp/sidenav.py
@@ -10,7 +10,7 @@ def init_app(app):
     )
 
     app.sidenav(
-        name="CP Images",
+        name="PC Images",
         url="https://www.cpimages.com/CS.aspx?VP3=CMS3&VF=Home&LANGSWI=1&LANG=French",
         icon="photo",
         group=8,
