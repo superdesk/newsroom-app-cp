dcf7d41878d11fe723f4b0cc50958f12f6c74d88
dev-tsoni <tanuj.soni@sourcefabric.org>
2021-12-21 18:11:49 +0530

    CPNHUB-7 ADD: Client for token auth.

--- server/cp/mgmt_api/auth_server/client.py
+++ server/cp/mgmt_api/auth_server/client.py
@@ -1,25 +1,9 @@
 import superdesk
 from . import oauth2
 from superdesk.auth_server.scopes import allowed_scopes
+from superdesk.auth_server.clients import AuthServerClientsResource, AuthServerClientsService
 
 
 def init_app(app):
     oauth2.config_oauth(app)
     superdesk.register_resource("auth_server_clients", AuthServerClientsResource, AuthServerClientsService, _app=app)
-
-
-class AuthServerClientsResource(superdesk.Resource):
-
-    schema = {
-        "name": {
-            "type": "string",
-            "required": True,
-            "unique": True,
-        },
-        "password": {"type": "string", "required": True},
-        "scope": {"type": "list", "allowed": list(allowed_scopes), "required": True},
-    }
-
-
-class AuthServerClientsService(superdesk.Service):
-    """Service to handle authorization server clients"""
