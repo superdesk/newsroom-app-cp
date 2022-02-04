de8e10b98ad987ce9642bb57bb2f9c108d901ce0
dev-tsoni <tanuj.soni@sourcefabric.org>
2022-02-04 21:53:25 +0530

    NHUB-80 FIX: wrong year format in agenda preview.

--- server/settings.py
+++ server/settings.py
@@ -33,7 +33,7 @@ CLIENT_CONFIG.update(
                 "TIME_FORMAT": "HH:mm",
                 "DATE_FORMAT": "MMM Do, YYYY",
                 "COVERAGE_DATE_FORMAT": "MMM Do, YYYY",
-                "COVERAGE_DATE_TIME_FORMAT": "HH:mm MMM Do, YYYYY",
+                "COVERAGE_DATE_TIME_FORMAT": "HH:mm MMM Do, YYYY",
                 "DATE_FORMAT_HEADER": "EEEE, MMMM d, yyyy",
             },
             "fr_CA": {
