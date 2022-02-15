2924a867a5608165edafdc773d60973081c7053b
Petr Jasek <jasekpetr@gmail.com>
2022-02-15 11:13:39 +0100

    test authentication via behave

--- server/features/steps/steps.py
+++ server/features/steps/steps.py
@@ -1,1 +1,10 @@
 from newsroom.tests.steps import * # noqa
+from behave import given
+
+
+@given('empty auth token')
+def given_empty_auth_token(context):
+    context.headers = [
+        header for header in context.headers
+        if header[0] != 'Authorization'
+    ]
