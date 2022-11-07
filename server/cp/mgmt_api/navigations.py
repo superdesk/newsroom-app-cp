4a5c93efa9892615b2112602b907e2381a73b81e
Petr Jasek <jasekpetr@gmail.com>
2022-11-07 10:43:43 +0100

    move management api into submodule

--- server/cp/mgmt_api/navigations.py
+++
@@ -1,7 +0,0 @@
-from newsroom.navigations import NavigationsResource, NavigationsService
-import superdesk
-
-
-def init_app(app):
-    NavigationsResource.internal_resource = False
-    superdesk.register_resource('navigations', NavigationsResource, NavigationsService, _app=app)
--- server/cp/mgmt_api/products.py
+++
@@ -1,7 +0,0 @@
-from newsroom.products import ProductsResource, ProductsService
-import superdesk
-
-
-def init_app(app):
-    ProductsResource.internal_resource = False
-    superdesk.register_resource('products', ProductsResource, ProductsService, _app=app)
