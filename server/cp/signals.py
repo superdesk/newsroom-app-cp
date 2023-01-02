fbdaae234e5b830a105f29ecfb9dc7e39f137e05
Petr Jasek <jasekpetr@gmail.com>
2023-01-02 14:19:49 +0100

    copy `extra.correction` to `body_html`
    CPNHUB-176

--- server/cp/signals.py
+++ server/cp/signals.py
@@ -1,10 +1,14 @@
-
 import cp
 
 from newsroom.signals import publish_item
 
 
 def on_publish_item(sender, item, **kwargs):
+    copy_headline2_to_headline(item)
+    copy_correction_to_body_html(item)
+
+
+def copy_headline2_to_headline(item):
     try:
         headline = item["extra"][cp.HEADLINE2]
     except KeyError:
@@ -12,6 +16,14 @@ def on_publish_item(sender, item, **kwargs):
     else:
         if headline:
             item["headline"] = headline
+
+
+def copy_correction_to_body_html(item):
+    if item.get("extra") and item["extra"].get(cp.CORRECTION):
+        item.setdefault("body_html", "")
+        item["body_html"] = "{}\n<p>{}</p>".format(
+            item["body_html"], item["extra"][cp.CORRECTION]
+        )
 
 
 def init_app(app):
