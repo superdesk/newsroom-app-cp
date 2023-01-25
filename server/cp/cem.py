d764e5e1c7440caa8646f80e700155bf93d95cee
Petr Jasek <jasekpetr@gmail.com>
2023-01-25 14:34:28 +0100

    avoid name conflicts

--- server/cp/cem.py
+++ server/cp/cem.py
@@ -6,15 +6,15 @@ from flask import current_app as app
 session = requests.Session()
 
 
-def send_notification(type, id):
+def send_notification(_type, _id):
     url = app.config.get("CEM_URL")
     if not url:
         return
     session.post(
         url,
         json={
-            "type": type,
-            "object_id": str(id),
+            "type": _type,
+            "object_id": str(_id),
             "platform": app.config.get("CEM_PLATFORM"),
         },
         timeout=5,
