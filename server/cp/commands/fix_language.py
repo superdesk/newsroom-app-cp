1965b11d361651a09ed09ed134dcdf202ef97fc1
Petr Jasek <jasekpetr@gmail.com>
2023-07-27 14:12:23 +0200

    add manage command to fix language
    it will remove `-CA` or `-US` from language.
    
    CPNHUB-227

---
+++ server/cp/commands/fix_language.py
@@ -0,0 +1,23 @@
+
+import time
+
+from superdesk import get_resource_service
+from newsroom.commands.manager import manager
+
+
+@manager.command
+def fix_language(resource="items", limit=50, sleep_secs=2):
+    service = get_resource_service(resource)
+
+    source = {"query": {"terms": {"language": ["en-CA", "en-US", "fr-CA"]}}, "size": 100}
+
+    for i in range(int(limit)):
+        items = service.search(source)
+        if not items.count():
+            break
+        for item in items:
+            updates = {"language": item["language"].split("-")[0]}
+            service.system_update(item["_id"], updates, item)
+        print(".", end="", flush=True)
+        time.sleep(int(sleep_secs))
+    print(".")
