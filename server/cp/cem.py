0a88e858a871726b4bfb7dfc49a9feb148caf228
Petr Jašek <jasekpetr@gmail.com>
2023-07-31 14:17:50 +0200

    change request to cem when user is created (#119)
    use email instead of id
    
    CPNHUB-247

--- server/cp/cem.py
+++ server/cp/cem.py
@@ -1,6 +1,7 @@
 import logging
 import requests
 
+from typing import Literal
 from flask import current_app as app
 
 
@@ -8,7 +9,7 @@ logger = logging.getLogger(__name__)
 session = requests.Session()
 
 
-def send_notification(_type, user):
+def send_notification(_type, user, id_key: Literal["_id", "email"] = "_id"):
     url = app.config.get("CEM_URL", "")
     apikey = app.config.get("CEM_APIKEY", "")
     if not url or not apikey:
@@ -16,7 +17,7 @@ def send_notification(_type, user):
     headers = {"x-api-key": apikey}
     payload = {
         "type": _type,
-        "object_id": str(user["_id"]),
+        "object_id": str(user[id_key]),
         "platform": app.config.get("CEM_PLATFORM"),
     }
     if user.get("company"):
