f94b7d2093efbfd744e9827bd470058c7116caa0
Petr Jasek <jasekpetr@gmail.com>
2023-10-02 15:17:22 +0200

    update config
    - disable contacts in agenda list
    - disable ranking/service from filters
    
    CPCN-350 CPCN-356

--- server/settings.py
+++ server/settings.py
@@ -154,21 +154,8 @@ WIRE_GROUPS = [
         },
     },
     {
-        "field": "distribution",
-        "label": lazy_gettext("Service"),
-        "nested": {
-            "parent": "subject",
-            "field": "scheme",
-            "value": "distribution",
-        },
-    },
-    {
         "field": "genre",
         "label": lazy_gettext("Version"),
-    },
-    {
-        "field": "urgency",
-        "label": lazy_gettext("Ranking"),
     },
 ]
 
@@ -178,7 +165,6 @@ WIRE_AGGS = {
     "service": {"terms": {"field": "service.name", "size": 50}},
     "subject": {"terms": {"field": "subject.name", "size": 50}},
     "genre": {"terms": {"field": "genre.name", "size": 50}},
-    "urgency": {"terms": {"field": "urgency"}},
 }
 
 AGENDA_GROUPS = [
