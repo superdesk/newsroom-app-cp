6497627b4ab42375e13ae1f0ed3d02c3f1fbfb12
devketanpro <devketanpro11@gmail.com>
2024-08-14 10:02:49 +0530

    update agenda filters [CPCN-910]

--- server/settings.py
+++ server/settings.py
@@ -415,13 +415,21 @@ SUPPORT_EMAIL_FR = SUPPORT_EMAIL_EN
 NOTIFY_MATCHING_USERS = "cancel"
 
 AGENDA_TIME_FILTERS = [
-    {"name": lazy_gettext("Last 24 hours"), "query": "now-24h/h"},
+    {"name": lazy_gettext("Next 7 days"), "filter": "next_7_days", "query": "now+7d/d"},
     {
-        "name": lazy_gettext("Last 7 days"),
-        "query": "now-7d/d",
+        "name": lazy_gettext("Next 30 days"),
+        "filter": "next_30_days",
+        "query": "now+30d/d",
     },
     {
-        "name": lazy_gettext("Last 30 days"),
-        "query": "now-30d/d",
+        "name": lazy_gettext("Next 3 months"),
+        "filter": "next_3_months",
+        "query": "now+3M/d",
+    },
+    {
+        "name": lazy_gettext("Next 12 months"),
+        "filter": "next_12_months",
+        "query": "now+12M/d",
+        "default": True,
     },
 ]
