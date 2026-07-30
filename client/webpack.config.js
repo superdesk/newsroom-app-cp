f636685a4c405f9c12a94aac2306c717a8360ae5
Taylor Li <t4ylor.li@gmail.com>
2026-07-30 14:35:46 -0400

    extract env for plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,21 @@ const config = require("newsroom-core/webpack.config");
 const dotenv = require("dotenv");
 const webpack = require("webpack");
 
+const KEYS = [
+  "AUTH0_DOMAIN",
+  "AUTH0_CLIENT_ID",
+  "AUTH0_REDIRECT_URI",
+  "AUTH0_FIREBASE_CONNECTION",
+];
+
+const parsed = dotenv.config().parsed ?? {};
+const env = KEYS.reduce((acc, key) => {
+  if (parsed[key] !== undefined) {
+    acc[key] = parsed[key];
+  }
+  return acc;
+}, {});
+
 config.entry.home_js = [
   config.entry.home_js,
   "./assets/auth/pr-manager-sso.ts",
@@ -11,7 +26,7 @@ config.entry.firebase_login_js = "./assets/auth/firebase/login.ts";
 config.plugins = [
   ...(config.plugins || []),
   new webpack.DefinePlugin({
-    "process.env": JSON.stringify(dotenv.config().parsed),
+    "process.env": JSON.stringify(env),
   }),
 ];
 
